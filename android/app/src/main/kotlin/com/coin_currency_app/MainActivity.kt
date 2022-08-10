8271db922c056266796834476a7b444c1c89d11f
Khurshid Ismoilov <95979088+khurshid28@users.noreply.github.com>
2022-08-09 22:30:47 -0700

    Add files via upload

---
+++ android/app/src/main/kotlin/com/coin_currency_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.khurshid28.coin_currency_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/khurshid28/coin_currency/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.khurshid28.coin_currency
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
